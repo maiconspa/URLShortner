1d041ec1d945ab69a24cc5ae0d6697c7815c432a
Maicon Souza <maiconsouzapa@gmail.com>
2025-09-14 01:21:25 -0300

    connection to S3

--- src/main/java/com/maiconspa/urlShortener/Main.java
+++ src/main/java/com/maiconspa/urlShortener/Main.java
@@ -1,4 +1,4 @@
-package com.maiconspa.urlShortner;
+package com.maiconspa.urlShortener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,11 +8,15 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
     // Jackson serialization library
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
@@ -29,7 +33,25 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
         // Extracting key values from deserialized Map
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
+        long expirationTimeInSeconds = Long.parseLong(expirationTime);
+
         String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        UrlData urlData = new UrlData(originalUrl, expirationTimeInSeconds);
+
+        try {
+            String urlDataJson = objectMapper.writeValueAsString(urlData);
+
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket("mspa-storage-urlshortener")
+                    .key(shortUrlCode + ".json")
+                    .build();
+
+            s3Client.putObject(request, RequestBody.fromString(urlDataJson));
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error saving data to S3: " + e.getMessage(), e);
+        }
 
         Map<String, String> response = new HashMap<>();
         response.put("code", shortUrlCode);
